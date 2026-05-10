ab8d107c01851e1a95f7d373faa8801d5a503421
cangerana <j.cangerana@outlook.com>
2026-05-10 05:36:01 -0300

    feat: implement default user and onboarding flow

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_19_220100) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_10_075555) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
@@ -20,7 +20,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_19_220100) do
     t.string "email", null: false
     t.string "name", null: false
     t.string "password_digest", null: false
-    t.integer "role", null: false
+    t.integer "role", default: 3, null: false
     t.integer "token_version", default: 0, null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
