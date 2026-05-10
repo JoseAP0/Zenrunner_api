f567ef872052852d31f7b4c299a08823c36d117b
cangerana <j.cangerana@outlook.com>
2026-05-10 05:36:01 -0300

    feat: implement default user and onboarding flow

---
+++ app/controllers/api/v1/auth/onboardings_controller.rb
@@ -0,0 +1,24 @@
+module Api
+  module V1
+    module Auth
+      class OnboardingsController < ApplicationController
+        include Authenticatable
+
+        def update
+          if current_user.update(onboarding_params)
+            render json: { data: { user: UserSerializer.new(current_user).as_json } }, status: :ok
+          else
+            render json: { errors: current_user.errors.full_messages }, status: :unprocessable_entity
+          end
+        end
+
+        private
+
+        def onboarding_params
+          # Permitting role and any other extra information that might be added to the user model later
+          params.require(:user).permit(:role)
+        end
+      end
+    end
+  end
+end
